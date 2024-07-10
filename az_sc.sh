5c63cc3b5d40049d5f887275111123c591936d16
Alex Schmid <alexandriaschmid3@gmail.com>
2024-07-09 22:46:02 -0400

    Add Supercloud batch script

---
+++ az_sc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -a 81-90
+#SBATCH --partition=xeon-p8
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=2-00:00
+#SBATCH -o /home/gridsan/aschmid/warehouse-task-assignment/outerr/wtow_%a.out
+#SBATCH -e /home/gridsan/aschmid/warehouse-task-assignment/outerr/wtow_%a.err
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=aschmid@mit.edu
+
+module load julia/1.9.2
+module load gurobi/gurobi-1102
+
+julia run_partitions.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
