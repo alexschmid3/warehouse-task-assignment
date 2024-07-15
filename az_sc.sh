554aa568f939f12fa18457c31313e73d8a938fe2
Alex Schmid <alexandriaschmid3@gmail.com>
2024-07-14 23:16:46 -0400

    Rerun multistop partitions

--- az_sc.sh
+++ az_sc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -a 1-4
+#SBATCH -a 212-221
 #SBATCH --partition=xeon-p8
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=2-00:00
-#SBATCH -o /home/gridsan/aschmid/warehouse-task-assignment/outerr/slots_%a.out
-#SBATCH -e /home/gridsan/aschmid/warehouse-task-assignment/outerr/slots_%a.err
+#SBATCH -o /home/gridsan/aschmid/warehouse-task-assignment/outerr/multistop_%a.out
+#SBATCH -e /home/gridsan/aschmid/warehouse-task-assignment/outerr/multistop_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=aschmid@mit.edu
 
