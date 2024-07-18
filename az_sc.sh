3e1ddcd768e23eb5a5c3e41548cb8bf60ee76e5c
Alex Schmid <alexandriaschmid3@gmail.com>
2024-07-18 14:31:14 -0400

    Main runs on sc

--- az_sc.sh
+++ az_sc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -a 480,482,484,486,488,490,492,494,496,498,500,502,504,506,508,510,512,514,516,518,520,522,524,526,528,530,532,534,536,538
+#SBATCH -a 1-10
 #SBATCH --partition=xeon-p8
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=16G
-#SBATCH --time=0-03:00
-#SBATCH -o /home/gridsan/aschmid/warehouse-task-assignment/outerr/maintrain_%a.out
-#SBATCH -e /home/gridsan/aschmid/warehouse-task-assignment/outerr/maintrain_%a.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=0-06:00
+#SBATCH -o /home/gridsan/aschmid/warehouse-task-assignment/outerr/mainrun_%a.out
+#SBATCH -e /home/gridsan/aschmid/warehouse-task-assignment/outerr/mainrun_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=aschmid@mit.edu
 
 module load julia/1.9.2
 module load gurobi/gurobi-1102
 
-julia run_makedata.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
+julia run_partitions.jl $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
