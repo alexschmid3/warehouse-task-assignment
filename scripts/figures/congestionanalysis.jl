d17263af264fc23b3bb082846243cc0d00d8d6ae
Alex Schmid <alexandriaschmid3@gmail.com>
2024-07-20 00:04:27 -0400

    More main runs

--- scripts/figures/congestionanalysis.jl
+++ scripts/figures/congestionanalysis.jl
@@ -1,6 +1,9 @@
 
 function congestionanalysis(nocongestion_flag)
 
+    l = maps.mapintersectiontorow[65]
+    totalcong = sum(currcong[p] for p in  partitioninfo[1].pods)
+
     allcongestionutils = []
     intersectionviolations, maxtraffic = Dict(), Dict()
     for i in globalpartition.intersections[1]
@@ -11,24 +14,24 @@ function congestionanalysis(nocongestion_flag)
     for s in 1:numpartitions
         currpartition = partitioninfo[s]
         for i in currpartition.intersections, t in 0:congestiontstep:horizon
-            totalutil = min(1,sum(currcong[p][maps.mapintersectiontorow[i],maps.maptimetocolumn[t]] for p in currpartition.pods) / intersectionmaxpods[i])
+            totalutil = sum(currcong[p][maps.mapintersectiontorow[i],maps.maptimetocolumn[t]] for p in currpartition.pods) / intersectionmaxpods[i]
             push!(allcongestionutils, totalutil)
             maxtraffic[i] = max(maxtraffic[i], totalutil)
-            #if totalutil > 1 + 1e-4
-            #    intersectionviolations[i] += 1
-            #end
+            if totalutil > 1 + 1e-4
+                intersectionviolations[i] += 1
+            end
         end
     end
     
     df = DataFrame(congutil = allcongestionutils)
 
     if nocongestion_flag == 0
-        CSV.write("figures/congestionanalysis/congestionsolution.csv", df)
+        CSV.write("figures/congestionanalysis/congestionsolution2.csv", df)
     else nocongestion_flag == 1
-        CSV.write("figures/congestionanalysis/nocongestionsolution.csv", df)
+        CSV.write("figures/congestionanalysis/nocongestionsolution2.csv", df)
     end
 
     include("scripts/visualizations/warehouseviz.jl")
-    warehouseviz("figures/congestionanalysis/warehousecongestion_us.png", 4000)
+    warehouseviz("figures/congestionanalysis/warehousecongestion_nocong.png", 4000)
 
 end
